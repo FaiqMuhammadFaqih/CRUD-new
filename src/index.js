9bbaf87ae4d7be3a53a7cdd69378b014c5a15e38
Faiq Muhammad Faqih <faiqmuhammadfaqih3@gmail.com>
2023-05-04 13:05:59 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import List from './List';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'remixicon/fonts/remixicon.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
